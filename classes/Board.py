86db95a16eb4ea43265ffd89537bc3422e832c3a
pbrdys <142018441+pbrdys@users.noreply.github.com>
2024-01-21 17:04:53 +0100

    Implement place ship on board

--- classes/Board.py
+++ classes/Board.py
@@ -1,4 +1,6 @@
 from colorama import Fore, Style 
+from classes.Orientation import Orientation
+import random
 
 class Board:
     """
@@ -27,7 +29,53 @@ class Board:
         self.already_sunk_ships = []
 
     def place_ship(self, ship):
-        print("place_ship")
+        """
+        Place a ship on the board randomly.
+
+        Parameters:
+        - ship (Ship): The ship to be placed on the board.
+        """
+
+        ship.positions = []  # Reset positions
+        ship.hits = []       # Reset hits
+
+        while True:
+            # Choose a random orientation for the ship (horizontal or vertical)
+            random_number = random.randint(0, 1)
+            if random_number == 1:
+                orientation = Orientation.VERTICAL
+            else:
+                orientation = Orientation.HORIZONTAL
+
+            # Generate random coordinates x, y for the ship's starting position
+            x = random.randint(0, self.width - 1)
+            y = random.randint(0, self.height - 1)
+
+            # Check if the ship fits in the chosen orientation and starting position within the board boundaries
+            is_horizontal = orientation == Orientation.HORIZONTAL
+            fits_horizontally = x + ship.ship_type.value <= self.width
+            is_vertical = orientation == Orientation.VERTICAL
+            fits_vertically = y + ship.ship_type.value <= self.height
+
+            if is_horizontal and fits_horizontally:
+                # Generate positions for a horizontally placed ship
+                positions = [(x + i, y) for i in range(ship.ship_type.value)]
+            elif is_vertical and fits_vertically:
+                # Generate positions for a vertically placed ship
+                positions = [(x, y + i) for i in range(ship.ship_type.value)]
+            else:
+                # Try again if the ship doesn't fit in the chosen orientation
+                continue
+
+            overlap = any(
+                pos in ship.positions
+                for pos in positions
+                for ship in self.ships
+            )
+            if not overlap:
+                ship.positions = positions
+                self.ships.append(ship)
+                break
 
     def receive_attack(self, x, y):
         print("receive_attack")
