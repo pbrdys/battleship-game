50567a839dade59d85acd547e56121a0150af06c
pbrdys <142018441+pbrdys@users.noreply.github.com>
2024-01-21 16:57:36 +0100

    Implement Game.start_game and end_game

--- classes/Game.py
+++ classes/Game.py
@@ -1,5 +1,6 @@
 from classes.Board import Board
 from classes.Player import Player
+from colorama import Fore, Style
 
 class Game:
     """
@@ -19,10 +20,60 @@ class Game:
         self.turn = 0
 
     def start_game(self):
-        print("start_game")
+        """
+        Start the Battleship game, allowing players to place their ships and initiating turns.
+        """
+
+        # Display a welcome message and game instructions
+        print("*" * 67)
+        print("**                 Welcome to Battleship Game!             **")
+        print("**                                                         **")
+        print("**  Prepare for an exciting naval battle against the       **")
+        print("**  computer opponent. Strategically place your fleet,     **")
+        print("**  unleash powerful attacks, and sink your opponent's     **")
+        print("**  ships to claim victory. Enjoy the thrill of the high   **")
+        print("**  seas right from your console!                          **")
+        print("*" * 67)
+        print("\n")
+        print("*" * 67)
+        print("**                 Are you ready to fight pirate?          **")
+        print("-" * 67)
+        print("**  1 Ready the cannons (start game)                       **")
+        print("**  2 Every man for himself! Swim for yer lives (end game) **")
+        print("*" * 67)
+
+        # Allow players to choose between starting the game or ending it
+        while True:
+            try:
+                # Get the player's choice
+                start_action = int(input("Enter your choice: \n"))
+
+                if start_action == 1:
+                    print("starting the game")
+                    break
+                elif start_action == 2:
+                    # end the game
+                    self.end_game()
+                    break
+                else:
+                    raise ValueError
+            except ValueError:
+                print(Fore.RED + "Invalid input. Please enter a valid integer (1, 2).")
+                print(Style.RESET_ALL)
+                continue
+            except Exception:
+                print(Fore.GREEN + "Your random values can not crash this battleship.")
+                print("Try again and make your move ;) \n")
+                print(Style.RESET_ALL)
+                continue
 
     def end_game(self):
-        print("end_game")
+        """
+        Quit the console
+        """
+        print(Fore.YELLOW + "You quit the game! Enjoy your day outside!")
+        print(Style.RESET_ALL)
+        quit()
 
     def place_ships_for_player(self, player):
         print("place_ships_for_player")
